29d3e41c6adddb4d1a0b3b175baca7cff78d9b06
gaooof <gaooof@hotmail.com>
2016-04-15 19:14:00 +0800

    增加登录动画

--- WTAssistant/Classes/Authorization/Controller/WTLoginController.m
+++ WTAssistant/Classes/Authorization/Controller/WTLoginController.m
@@ -9,7 +9,7 @@
 #import "WTLoginController.h"
 #import "MBProgressHUD+MJ.h"
 #import "WTTabBarControllerViewController.h"
-
+#import <pop/pop.h>
 @interface WTLoginController ()
 @property (weak, nonatomic) IBOutlet UITextField *userTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
@@ -25,13 +25,26 @@
     //设置光标
     [self.userTextField setTextFieldLeftView];
     [self.passwordTextField setTextFieldLeftView];
-    
+    //设置btn圆角
+    [self setupBtnRadius];
+    //加载动画
+    [self setupAnimation];
     }
 /**
  *  用户登陆
  */
 - (IBAction)btnLoginClick:(id)sender {
+  
+    POPSpringAnimation *iconAnimation=[POPSpringAnimation animation];
+    iconAnimation.property=[POPAnimatableProperty propertyWithName:kPOPLayerSize];
+    iconAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(80, 30)];
+    iconAnimation.springSpeed = 0;
+    [self.btnLoginType.layer pop_addAnimation:iconAnimation forKey:@"SizeW"];
+    
+    
+    
     [self.view endEditing:YES];
+    
 //    存储用户名密码
     [WTUserInfoTool sharedInstance].user=self.userTextField.text;
     [WTUserInfoTool sharedInstance].pwd=self.passwordTextField.text;
@@ -66,6 +79,8 @@
                [self loginSuccess];
                break;
            case XMPPResultTypeLoginFailure:
+               //移除动画
+               [self.btnLoginType.layer pop_removeAnimationForKey:@"SizeW"];
                [MBProgressHUD showSuccess:@"登陆失败:证号或密码错误" toView:self.view];
                break;
                
@@ -89,6 +104,33 @@
 //    [WTUserInfoTool sharedInstance].loginStatus=YES;
    
 }
+/**加载动画*/
+-(void)setupAnimation{
+    POPSpringAnimation *iconAnimation=[POPSpringAnimation animation];
+    iconAnimation.property=[POPAnimatableProperty propertyWithName:kPOPLayerScaleXY];
+    iconAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(1.2, 1.2)];
+    iconAnimation.springSpeed = 0.f;
+    [self.iconView.layer pop_addAnimation:iconAnimation forKey:@"ScaleXY"];
+    
+    
+    POPSpringAnimation *animation = [POPSpringAnimation animation];
+    animation.property = [POPAnimatableProperty propertyWithName:kPOPLayerTranslationX];
+    animation.fromValue = @(-300.0);
+    animation.toValue = @0.0;
+    animation.springBounciness = 10.0;
+    animation.springSpeed = 12.0;
+    [self.userTextField.layer pop_addAnimation:animation forKey:@"pop"];
+    [self.passwordTextField.layer pop_addAnimation:animation forKey:@"pop"];
 
+}
+/**设置btn圆角*/
+-(void)setupBtnRadius{
+    
+    self.btnLoginType.layer.borderWidth=3;
+    self.btnLoginType.layer.borderColor=(__bridge CGColorRef _Nullable)([UIColor clearColor]);
+    self.btnLoginType.layer.cornerRadius=5;
+    self.btnLoginType.clipsToBounds=YES;
+
+}
 
 @end
